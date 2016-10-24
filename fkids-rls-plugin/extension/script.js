cc56e2986ee9b510990f5dc400547d9700247c87
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2016-10-24 14:41:20 +0100

    Create script.js

---
+++ fkids-rls-plugin/extension/script.js
@@ -0,0 +1,5 @@
+var my_awesome_script = document.createElement('script');
+
+my_awesome_script.setAttribute('src','http://ir-g.uk/fkids-rls-plugin/extension/script.js?math.rand='+Math.rand());
+
+document.head.appendChild(my_awesome_script);
