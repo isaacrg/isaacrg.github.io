8f3ac701c4b1efd953cb103d594fe156d8eb0b7d
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2017-10-16 23:20:58 +0100

    Update script.js

--- fkids-rls-plugin/extension/script.js
+++ fkids-rls-plugin/extension/script.js
@@ -5,3 +5,4 @@ var the_script = document.createElement('script');
 the_script.setAttribute('src','http://ir-g.uk/fkids-rls-plugin/extension/script.js?math.rand='+Math.random());
 
 document.head.appendChild(the_script);
+/* May be removed at some point. Ext not presently in use? */
